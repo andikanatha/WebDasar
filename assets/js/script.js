97ccb76c0795f5c1199bd0b162b070a17c8b51fc
andikanatha <setya.natha@gmail.com>
2022-09-28 07:10:24 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const body = document.querySelector("body");
+const nav = document.querySelector("nav");
+const navList = document.querySelector(".nav-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = ()=>{
+    navList.classList.add("active");
+    menuBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+    body.classList.add("disabledScroll");
+}
+
+cancelBtn.onclick = ()=>{
+    navList.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    body.classList.remove("disabledScroll");
+}
+
+const dropDown = document.querySelector(".dropdown"); 
+const subMenu = document.querySelector(".submenu"); 
+
+dropDown.addEventListener('click', function(e) {
+    if(subMenu.classList.contains('submenu-hide')) {
+        subMenu.classList.add("submenu-show");
+        subMenu.classList.remove("submenu-hide");
+    }else if(subMenu.classList.contains('submenu-show')){
+        subMenu.classList.remove("submenu-show");
+        subMenu.classList.add("submenu-hide");
+    }
+    
+    e.preventDefault();
+});
+
+window.onscroll = ()=>{
+    this.scrollY > 20 ? nav.classList.add("sticky") : nav.classList.remove("sticky");
+}
+
+$('.page-scroll').on('click', function(e){
+
+    let tujuan = $(this).attr('href');
+    let elemenTujuan = $(tujuan);
+
+    $('html').animate({
+        scrollTop: elemenTujuan.offset().top - 62
+    }, 1000)
+
+    e.preventDefault();
+})
